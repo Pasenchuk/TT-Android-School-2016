39da40f78366600c6a38608aa4db4cc666fad6a5
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-02-01 01:14:06 +0600

    Кнопка нам на самом деле не нужна

--- app/src/main/java/com/ucsoftworks/tt_android_school_2016/MainFragment.java
+++ app/src/main/java/com/ucsoftworks/tt_android_school_2016/MainFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -21,8 +20,6 @@ public class MainFragment extends Fragment {
 
     @Bind(R.id.label)
     TextView label;
-    @Bind(R.id.button)
-    Button button;
 
     public MainFragment() {
         // Required empty public constructor
