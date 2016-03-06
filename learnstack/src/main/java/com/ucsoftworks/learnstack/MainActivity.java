e8b7723c8d0275f8dc8f24d5b55ec072af2fe1d2
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-03-07 04:14:35 +0600

    Load fragment in activity.

--- learnstack/src/main/java/com/ucsoftworks/learnstack/MainActivity.java
+++ learnstack/src/main/java/com/ucsoftworks/learnstack/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ucsoftworks.learnstack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public void onActivityStackButtonPressed() {
+
     }
 }
