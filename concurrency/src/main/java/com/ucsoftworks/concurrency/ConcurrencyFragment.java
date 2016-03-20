068b63f53aaf0350fd285b53e682304457492a1b
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-03-20 08:19:32 +0600

    Timer & Rx timer.

--- concurrency/src/main/java/com/ucsoftworks/concurrency/ConcurrencyFragment.java
+++ concurrency/src/main/java/com/ucsoftworks/concurrency/ConcurrencyFragment.java
@@ -14,10 +14,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 /**
@@ -26,6 +33,7 @@ import butterknife.OnClick;
 public class ConcurrencyFragment extends Fragment {
 
 
+    public static final int EMITS_COUNT = 5;
     @Bind(R.id.message)
     TextView message;
 
@@ -34,6 +42,7 @@ public class ConcurrencyFragment extends Fragment {
 
     private Handler handler = new Handler();
     private AsyncTask<Double, Integer, String> asyncTask;
+    private Timer timer;
 
     public ConcurrencyFragment() {
         // Required empty public constructor
@@ -65,6 +74,10 @@ public class ConcurrencyFragment extends Fragment {
         super.onStop();
         if (asyncTask != null && !asyncTask.isCancelled())
             asyncTask.cancel(true);
+
+        if (timer != null)
+            timer.cancel();
+
     }
 
     @OnClick({R.id.runnable, R.id.new_thread, R.id.handler_post, R.id.async_task, R.id.timer_task, R.id.rx})
@@ -90,7 +103,7 @@ public class ConcurrencyFragment extends Fragment {
                         for (int i = 1; i <= 10; i++) {
                             SystemClock.sleep(1000);
                             final String text = String.valueOf(i);
-                            postProgress(i * 10);
+                            progressBar.setProgress(i * 10);
                             postText(text);
                         }
                     }
@@ -140,8 +153,58 @@ public class ConcurrencyFragment extends Fragment {
 
                 break;
             case R.id.timer_task:
+
+                message.setText("Timer started");
+
+                if (timer != null)
+                    timer.cancel();
+
+                timer = new Timer();
+
+
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (isVisible()) {
+                            progressBar.setProgress(100);
+
+                            postTextDirectly("Timer finished");
+                        }
+
+                    }
+                };
+
+                timer.schedule(timerTask, 2000);
                 break;
             case R.id.rx:
+
+                Observable
+                        .interval(1, TimeUnit.SECONDS)
+                        .take(EMITS_COUNT)
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<Long>() {
+                            @Override
+                            public void onCompleted() {
+                                if (isVisible()) {
+                                    progressBar.setProgress(100);
+                                    message.setText("Rx finished");
+                                }
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                            }
+
+                            @Override
+                            public void onNext(Long aLong) {
+                                if (isVisible()) {
+                                    progressBar.setProgress((int) (aLong * 100 / EMITS_COUNT));
+                                    message.setText(String.valueOf(aLong));
+                                }
+                            }
+                        });
                 break;
         }
     }
@@ -152,16 +215,6 @@ public class ConcurrencyFragment extends Fragment {
                 @Override
                 public void run() {
                     message.setText(text);
-                }
-            });
-    }
-
-    private void postProgress(final int progress) {
-        if (isVisible())
-            progressBar.post(new Runnable() {
-                @Override
-                public void run() {
-                    progressBar.setProgress(progress);
                 }
             });
     }
