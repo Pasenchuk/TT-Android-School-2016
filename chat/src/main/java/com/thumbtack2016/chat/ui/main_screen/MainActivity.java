06fc1dc9317f4a0987d5d92046b9b25fee6112f5
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-04-10 05:37:43 +0600

    Add api call.

--- chat/src/main/java/com/thumbtack2016/chat/ui/main_screen/MainActivity.java
+++ chat/src/main/java/com/thumbtack2016/chat/ui/main_screen/MainActivity.java
@@ -6,9 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import com.thumbtack2016.chat.R;
 import com.thumbtack2016.chat.app.App;
 import com.thumbtack2016.chat.network.AuthApi;
+import com.thumbtack2016.chat.network.models.Auth;
+import com.thumbtack2016.chat.network.models.Token;
 import com.thumbtack2016.chat.ui.dialogs.AuthDialog;
 
 import javax.inject.Inject;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         App.getApp(this).getAppComponent().inject(this);
-        
+
+        new AuthDialog(this)
+                .show(getString(R.string.please_auth), "", "")
+                .flatMap(new Func1<Auth, Observable<Token>>() {
+                    @Override
+                    public Observable<Token> call(Auth auth) {
+                        return authApi.getToken(auth);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Token>() {
+                    @Override
+                    public void call(Token token) {
+
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
+                    }
+                });
     }
 }
