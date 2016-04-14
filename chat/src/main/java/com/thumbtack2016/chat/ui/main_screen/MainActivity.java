d4c38eedd4d67673420c828275a542ed4d5c9fd9
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-04-14 15:01:29 +0600

    Add play services check.

--- chat/src/main/java/com/thumbtack2016/chat/ui/main_screen/MainActivity.java
+++ chat/src/main/java/com/thumbtack2016/chat/ui/main_screen/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.thumbtack2016.chat.R;
 import com.thumbtack2016.chat.app.App;
 import com.thumbtack2016.chat.app.AppPreferences;
@@ -26,6 +28,8 @@ import rx.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_IS_FROM_NOTIFICATION = "KEY_IS_FROM_NOTIFICATION";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
     @Inject
     AuthApi authApi;
 
@@ -40,14 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         App.getApp(this).getAppComponent().inject(this);
 
-
-        if (appPreferences.getGcmId() != null)
-            Log.d("GCM ID", appPreferences.getGcmId());
-        else {
-            Log.d("GCM ID", "not created");
-
-            startService(new Intent(this, RegistrationIntentService.class));
-        }
 
         new AuthDialog(this)
                 .show(getString(R.string.please_auth), "", "")
@@ -65,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
                     public void call(Token token) {
                         MessageBox.show(token.getToken(), MainActivity.this);
 
+                        if (checkPlayServices()) {
+                            if (appPreferences.getGcmId() != null)
+                                Log.d("GCM ID", appPreferences.getGcmId());
+                            else {
+                                Log.d("GCM ID", "not created");
+
+                                startService(new Intent(MainActivity.this, RegistrationIntentService.class));
+                            }
+                        }
+
                     }
                 }, new Action1<Throwable>() {
                     @Override
@@ -73,5 +79,27 @@ public class MainActivity extends AppCompatActivity {
                         MessageBox.show(getString(R.string.network_error), MainActivity.this);
                     }
                 });
+    }
+
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                MessageBox.show(R.string.no_play_services, this);
+            }
+            return false;
+        }
+        return true;
     }
 }
