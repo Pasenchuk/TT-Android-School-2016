2d2840e5f6b637fa73b7ff6b526489de908899ba
Pasenchuk Victor <pasenchuk-victor@yandex.ru>
2016-04-24 17:19:59 +0600

    Add map box

--- geolocationapp/src/main/java/com/ucsoftworks/geolocationapp/MainActivity.java
+++ geolocationapp/src/main/java/com/ucsoftworks/geolocationapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.ucsoftworks.geolocationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this);
         setContentView(R.layout.activity_main);
     }
 }
